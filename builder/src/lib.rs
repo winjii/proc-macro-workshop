347b422af4a680c9e93579051f950ef8ff61ca84
winjii <wing3196@gmail.com>
2021-08-08 17:07:04 +0900

    builder test 1

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,10 @@
 use proc_macro::TokenStream;
+use syn;
+use syn::{parse_macro_input};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let input = parse_macro_input!(input as syn::DeriveInput);
+    eprintln!("INPUT: {:#?}", input);
+    TokenStream::new()
 }
