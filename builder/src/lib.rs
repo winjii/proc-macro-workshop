9488c3af1418baf0b84f426c3b6b4b07554496d7
winjii <wing3196@gmail.com>
2021-08-09 03:57:30 +0900

    Pass builder test 2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,43 @@
 use proc_macro::TokenStream;
-use syn;
-use syn::{parse_macro_input};
+use quote::quote;
+use syn::parse_macro_input;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as syn::DeriveInput);
     eprintln!("INPUT: {:#?}", input);
-    TokenStream::new()
+    let struct_name = input.ident;
+    let builder_name = format!("{}Builder", struct_name);
+    let builder_name = syn::Ident::new(&builder_name, struct_name.span());
+    let (field_names, field_types) = match input.data {
+        syn::Data::Struct(ref ds) => {
+            let fields = ds.fields.iter().filter_map(|field| {
+                field.ident.as_ref().map(|ident| { (ident, &field.ty) })
+            });
+            (
+                fields.clone().map(|(x, _)| x).collect::<Vec<&syn::Ident>>(),
+                fields.map(|(_, x)| x).collect::<Vec<&syn::Type>>()
+            )
+        }
+        _ => {
+            panic!("{} has data which is not struct.", struct_name);
+        },
+    };
+    
+    let output = quote! {
+        impl #struct_name {
+            pub fn builder() -> #builder_name {
+                #builder_name { #(#field_names: None),* }
+            }
+        }
+        pub struct #builder_name {
+            #(
+                #field_names: Option<#field_types>
+            ),*
+        }
+    };
+    let output = output.into();
+    eprintln!("OUTPUT: {:#}", output);
+    // eprintln!("attrs: {:#?}", attrs[0]);
+    output
 }
